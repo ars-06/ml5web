17ece72243680757b45e407895d2eade05a8a15a
ars-06 <79229631+ars-06@users.noreply.github.com>
2021-12-31 19:56:55 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -17,5 +17,29 @@ document.getElementById("Captured_img").innerHTML = '<img id="captured_image" sr
 }
 console.log('ml5 version', ml5.version);
 
-classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JZCUbMuAw/model.json',modelLoaded());
+classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Y2XauPsy9/model.json',modelLoaded());
 
+function modelLoaded(){
+    console.log("MODEL IS INITILIZED STARTING JET BOOSTERS IN: ");
+    console.log("3");
+    console.log("2");
+    console.log("1");
+    console.log("TAKEOFF");
+    }
+
+    function check(){
+        image = document.getElementById('captured_image')
+        Classifier.classify(image,gotResult);
+    }
+
+    function gotResult(error,results){
+if(error){
+console.log(error);
+window.alert("Error Detected !");
+}
+else{
+    console.log(results);
+document.getElementById("result_object_name").innerHTML = results[0].label;
+document.getElementById("result_object_accuracy").innerHTML = (results[0].confidence*100).toFixed(2)+"%";
+}
+    }
